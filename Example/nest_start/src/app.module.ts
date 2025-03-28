09c35f15379847da6934407892ad1752733af409
SongHanSol <skycriperd@gmail.com>
2025-03-28 18:01:37 +0900

    123

--- Example/nest_start/src/app.module.ts
+++ Example/nest_start/src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from './app.service';
 // .env 사용
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { User } from './user/entities/User.entity';
 
 @Module({
   imports: [
@@ -22,14 +23,13 @@ import { UserModule } from './user/user.module';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD, 
       database: process.env.DB_DATABASE,       // 연결할 데이터베이스 이름으로 변경
-      // entities: [User],                // 사용할 엔티티 클래스 목록을 배열로 직접 지정
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'], // 또는 __dirname을 사용하여 프로젝트 내 모든 .entity 파일 스캔 (권장)
-      synchronize: true, // DB 스키마 엔티티 따라 자동변경 => 개발에서만 사용
+      entities: [User],                // 사용할 엔티티 클래스 목록을 배열로 직접 지정
+      // entities: [__dirname + '/../**/*.entity{.ts,.js}'], // 또는 __dirname을 사용하여 프로젝트 내 모든 .entity 파일 스캔 (권장)
+      synchronize: true, // 개발에서만 사용 DB 스키마 자동변경
       // logging: true, // SQL 쿼리로깅
     }),
     UserModule,
     // 다른 모듈들...
-    // 예: UserModule, ProductModule 등
   ],
   controllers: [AppController],
   providers: [AppService],
